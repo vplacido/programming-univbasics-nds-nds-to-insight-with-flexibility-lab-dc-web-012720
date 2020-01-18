eaa82c50b07b5c84d22e10d9862a60519e6292ce
Valentin Placido <valentinplacido25@gmail.com>
2020-01-18 07:02:24 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -96,6 +96,9 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+  
+  arr = []
+  
 end
 
 # ----------------    End of Your Code Region --------------------
