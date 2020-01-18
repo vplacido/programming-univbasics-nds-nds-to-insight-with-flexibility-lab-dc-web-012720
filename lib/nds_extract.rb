3b5f3945bea6c389ae2e901e891031b9e51af2e2
Valentin Placido <valentinplacido25@gmail.com>
2020-01-18 07:03:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -100,7 +100,8 @@ def movies_with_directors_set(source)
   arr = []
   i = 0 
   while i < source.length do 
-    director = source[]
+    director = source[i]
+    directorName = 
 end
 
 # ----------------    End of Your Code Region --------------------
