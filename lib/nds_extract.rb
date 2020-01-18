363bb71e323318acc993200dd02d6e500a50d436
Valentin Placido <valentinplacido25@gmail.com>
2020-01-18 06:50:35 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -52,8 +52,9 @@ def movies_with_director_key(name, movies_collection)
   i = 0 
   while i < movies_collection.length do 
     newData = movies_collection[i]
-    data = movie_with_director_name()
-  data[name] = movie_with_director_name(name, movies_collection)
+    data = movie_with_director_name(name, newData)
+    i += 1 
+  end
   data
 end
 
